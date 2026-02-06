db519384ba1dc3fbb3fb6d66c5e09571a2ea46a8
beyout2025-tech <be.you.t2025@gmail.com>
2026-02-06 13:55:28 +0300

    Add main and chat script URLs to url.js

---
+++ js/url.js
@@ -0,0 +1,6 @@
+// رابط السكربت الأساسي (المسؤول عن تسجيل الدخول والحضور والبيانات)
+const WEB_APP_URL_Main = "https://script.google.com/macros/s/AKfycbxooDCcHIBQc_iZ4xqhdBw2KN_3Zsbc6HujWy_z-E01oZi1oNPdbh5jlV-0OwI2FnnI/exec";
+
+// رابط سكربت الشات (إذا كنت تستخدم سكربت منفصل للشات)
+const WEB_APP_URL_Chat = "https://script.google.com/macros/s/AKfycbxzvqKPrtIHrnTW6RKKgeBnvYNPHqLGZcvlpa2rOfM3FK055lW177aVEM62mo3y75y1/exec";
+
