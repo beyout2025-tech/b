ef3097cdc4095b476b56bcbff4f54e7abbff2bf6
beyout2025-tech <be.you.t2025@gmail.com>
2026-02-07 00:15:40 +0300

    Replace chat script URL with new API URL

--- js/url.js
+++ js/url.js
@@ -2,6 +2,6 @@
 const WEB_APP_URL_Main = "https://script.google.com/macros/s/AKfycbxb-PFmCM-EL3qZw26kOmKO4F3VKYp8I2DsvzcpklwL3YSitkfLy2EzCR6AUXyuYIvw/exec";
 
 // رابط سكربت الشات (إذا كنت تستخدم سكربت منفصل للشات)
-const WEB_APP_URL_Chat = "https://script.google.com/macros/s/AKfycbxbPagPiDpYM9SQQCfqHOHaoPKusr_uwRlNBHRvhX_s4lAGLgk3TH3UzK5PJwnnDYR6/exec";
+const API = "https://script.google.com/macros/s/AKfycbzvIF-kod-UJ2vKwXHMfIyL5fbZBy0FGDgh2vgxkxEVEwD5qeiehNKFFWrpUTQbze0L/exec";
 
 
