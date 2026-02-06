d684b3414127c7dd5f36b9615f300b9abc976462
beyout2025-tech <be.you.t2025@gmail.com>
2026-02-06 18:53:27 +0300

    Update chat script URL in url.js

--- js/url.js
+++ js/url.js
@@ -2,6 +2,6 @@
 const WEB_APP_URL_Main = "https://script.google.com/macros/s/AKfycbxb-PFmCM-EL3qZw26kOmKO4F3VKYp8I2DsvzcpklwL3YSitkfLy2EzCR6AUXyuYIvw/exec";
 
 // رابط سكربت الشات (إذا كنت تستخدم سكربت منفصل للشات)
-const WEB_APP_URL_Chat = "https://script.google.com/macros/s/AKfycby7nQeowecmBWx3u91Op4vE6x7rKMetaWEY2dFF5dV0KqP1cdMgt0gzZYQIrHgbnUiD/exec";
+const WEB_APP_URL_Chat = "https://script.google.com/macros/s/AKfycbxbPagPiDpYM9SQQCfqHOHaoPKusr_uwRlNBHRvhX_s4lAGLgk3TH3UzK5PJwnnDYR6/exec";
 
 
