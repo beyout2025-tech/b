0fb5061667603e04ecb6b6c9efcd585d8199a832
beyout2025-tech <be.you.t2025@gmail.com>
2026-02-06 18:16:53 +0300

    Update web app URLs in url.js

--- js/url.js
+++ js/url.js
@@ -1,6 +1,7 @@
 // رابط السكربت الأساسي (المسؤول عن تسجيل الدخول والحضور والبيانات)
-const WEB_APP_URL_Main = "https://script.google.com/macros/s/AKfycbxooDCcHIBQc_iZ4xqhdBw2KN_3Zsbc6HujWy_z-E01oZi1oNPdbh5jlV-0OwI2FnnI/exec";
+const WEB_APP_URL_Main = "https://script.google.com/macros/s/AKfycbxb-PFmCM-EL3qZw26kOmKO4F3VKYp8I2DsvzcpklwL3YSitkfLy2EzCR6AUXyuYIvw/exec";
 
 // رابط سكربت الشات (إذا كنت تستخدم سكربت منفصل للشات)
-const WEB_APP_URL_Chat = "https://script.google.com/macros/s/AKfycbxzvqKPrtIHrnTW6RKKgeBnvYNPHqLGZcvlpa2rOfM3FK055lW177aVEM62mo3y75y1/exec";
+const WEB_APP_URL_Chat = "https://script.google.com/macros/s/AKfycby7nQeowecmBWx3u91Op4vE6x7rKMetaWEY2dFF5dV0KqP1cdMgt0gzZYQIrHgbnUiD/exec";
+
 
